1ce745bffb339c0080348a29f524c890f1ca7d9b
moamentorky8 <166957985+moamentorky8@users.noreply.github.com>
2026-04-16 22:37:25 +0200

    Add Wi-Fi and Firebase configuration to config.py
    Added Wi-Fi and Firebase configuration settings.

--- firmware/config.py
+++ firmware/config.py
@@ -1,1 +1,7 @@
+# إعدادات الواي فاي
+WIFI_SSID = "اسم_الشبكة_بتاعتك"
+WIFI_PASS = "باسورد_الشبكة"
 
+# إعدادات الفايربيز
+FIREBASE_URL = "https://cureconnect-default-rtdb.firebaseio.com/"
+FIREBASE_AUTH = "Secret_Key_بتاعك"
