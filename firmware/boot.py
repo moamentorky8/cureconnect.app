93599c3eb05eb01e13ac33e18d516c8a6e4c44b7
moamentorky8 <166957985+moamentorky8@users.noreply.github.com>
2026-04-16 22:37:54 +0200

    Implement WiFi connection in boot.py
    Added a function to connect to WiFi using provided credentials.

--- firmware/boot.py
+++ firmware/boot.py
@@ -1,1 +1,15 @@
+import network
+import config
+import time
 
+def connect_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('Connecting to WiFi...')
+        wlan.connect(config.WIFI_SSID, config.WIFI_PASS)
+        while not wlan.isconnected():
+            time.sleep(1)
+    print('WiFi Connected! IP:', wlan.ifconfig()[0])
+
+connect_wifi()
